7d733b087885f930b0018f4cea988a9e88fc0fca
Walt Wengert <walt.frederick.john@live.com>
2026-03-15 20:23:53 +1000

    fix app paths

--- apps/portfolio/vite.config.ts
+++ apps/portfolio/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitest/config';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/',
+    base: '/ww-web-apps/',
     test: {
         globals: true,
         environment: 'jsdom',
--- apps/secret-santa/vite.config.ts
+++ apps/secret-santa/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitest/config';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/secret-santa/',
+    base: '/ww-web-apps/secret-santa/',
     test: {
         globals: true,
         environment: 'jsdom',
