0703608e78a37f1e57f304137539a2022222751b
Walt Wengert <walt.frederick.john@live.com>
2026-03-15 18:07:01 +1000

    migrate to font packages

--- apps/portfolio/src/main.tsx
+++ apps/portfolio/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './PortfolioApp';
 
+import '@fontsource/roboto-slab/400.css';
 import '../style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
